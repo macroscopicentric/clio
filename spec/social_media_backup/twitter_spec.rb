9ad70dd0ec5993f56e52c2875ce64bc825d262da
Rachel King <rachel.b.king@gmail.com>
2019-08-05 21:38:52 -0400

    Add test to document expected archive format

--- spec/social_media_backup/twitter_spec.rb
+++ spec/social_media_backup/twitter_spec.rb
@@ -1,18 +1,67 @@
 require_relative '../spec_helper'
 
 describe 'SocialMediaBackup::Twitter' do
+
+	let(:tweet_text) { 'Exciting stuff happened.' }
+	let(:created_at) { '2017-06-18' }
+	let(:media_url) { 'example.com/some_address' }
+
 	let(:tweet) do
 		{
-			id: '1',
-			created_at: '2017-6-18',
-			text: 'Exciting stuff happened.',
+			id: 1,
+			created_at: created_at,
+			text: tweet_text,
 			retweet: false,
 			original_tweet: '',
 			original_user: '',
 			reply: false,
 			reply_to: '',
-			media: []
+			media: [
+				{ type: :url, id: nil, url: media_url }
+			],
 		}
+	end
+
+	context '#format_csv_tweet' do
+
+		# The expected format for a tweet from the Twitter archive.
+		let(:csv_tweet) do
+			CSV::Row.new(
+				[
+					'tweet_id',
+					'in_reply_to_status_id',
+					'in_reply_to_user_id',
+					'timestamp',
+					'source',
+					'text',
+					'retweeted_status_id',
+					'retweeted_status_user_id',
+					'retweeted_status_timestamp',
+					'expanded_urls'
+				],
+				[
+					'1',
+					'',
+					'',
+					created_at,
+					'example.com',
+					tweet_text,
+					'',
+					'',
+					'',
+					"#{media_url},#{media_url}"
+				]
+			)
+		end
+
+		it "formats a csv tweet correctly" do
+			twitter_backup = SocialMediaBackup::Twitter.new
+			formatted_tweet = twitter_backup.format_csv_tweet(csv_tweet)
+
+			expect(formatted_tweet[:media].length).to be(1)
+			expect(formatted_tweet).to eq(tweet)
+		end
+
 	end
 
 	context '#merge_tweets' do
